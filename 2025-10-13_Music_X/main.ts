3d019a62cbe27ce756118342ee1af79baf5a65e5
KOLM Benjamin <126233198+KOLMBenjamin@users.noreply.github.com>
2025-10-20 19:23:55 +0200

    Abgabe

--- 2025-10-13_Music_X/main.ts
+++ 2025-10-13_Music_X/main.ts
@@ -1,13 +1,54 @@
 import { fakerDE_AT } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
 
-function main() {
-  const musik_faker = fakerDE_AT.music;
-  console.log("Random Music Genre:", musik_faker.genre());
-  console.log("Random Album:", musik_faker.album());
-  console.log("Random Song Name:", musik_faker.songName());
-  console.log("Random Artist:", musik_faker.artist());
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const artist = await prisma.artist.create({
+    data: {
+      name: fakerDE_AT.person.fullName(),
+      sprache: fakerDE_AT.location.country(),
+    },
+  });
+  console.log(`Artist: ${artist.name}`);
+
+  const album = await prisma.album.create({
+    data: {
+      title: fakerDE_AT.music.album(),
+      jahr: fakerDE_AT.number.int({ min: 1970, max: 2025 }),
+      artistId: artist.id,
+    },
+  });
+  console.log(`Album: ${album.title}`);
+
+  const genre = await prisma.genre.create({
+    data: {
+      name: fakerDE_AT.music.genre(),
+      erscheinungsjahr: fakerDE_AT.number.int({ min: 1950, max: 2025 }),
+    },
+  });
+  console.log(`Genre: ${genre.name}`);
+
+  for (let i = 0; i < 5; i++) {
+    const song = await prisma.song.create({
+      data: {
+        name: fakerDE_AT.music.songName(),
+        dauer: fakerDE_AT.number.int({ min: 120, max: 400 }),
+        albumId: album.id,
+        genreId: genre.id,
+      },
+    });
+    console.log(` Song ${i + 1}: ${song.name}`);
+  }
+
+  console.log("\n Fake-Daten eingefügt");
 }
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  main();
-}
+
+main()
+  .catch((err) => {
+    console.error("Fehler beim Einfügen", err);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
